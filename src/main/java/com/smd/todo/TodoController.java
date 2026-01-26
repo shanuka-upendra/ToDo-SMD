141dba2f357fe4bd4301c94d44022daf1a176c01
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-27 01:56:29 +0530

    feat : Implement CRUD Operations

--- src/main/java/com/smd/todo/TodoController.java
+++ src/main/java/com/smd/todo/TodoController.java
@@ -1,13 +1,38 @@
 package com.smd.todo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TodoController {
 
+    @Autowired
+    TodoRepository todoRepository;
+
     @GetMapping("/")
-    String hello(){
-        return "hello";
+    List<Todo> hello(){
+        return todoRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public Todo create(@RequestBody Todo todo){
+        return todoRepository.save(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        todoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Todo update(@PathVariable Long id, @RequestBody Todo todoDetails) {
+        Todo todo = todoRepository.findById(id).orElseThrow();
+
+        todo.setTitle(todoDetails.getTitle());
+        todo.setCompleted(todoDetails.getCompleted());
+
+        return todoRepository.save(todo);
     }
 }
